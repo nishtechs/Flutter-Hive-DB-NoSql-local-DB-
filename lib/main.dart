8f72c9774b5cba8769122e7edd977416695fb835
Nishant Sharma <65259474+nishtechs@users.noreply.github.com>
2023-04-17 18:25:35 +0530

    - First Commit, adding all files to git repo for first time

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_hive_db/todo.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -7,6 +8,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Directory directory = await getApplicationDocumentsDirectory();
   Hive.init(directory.path);
+  //Register the Adapter
+  Hive.registerAdapter(TodoAdapter());
+  //Open a new box with todo data type
+  await Hive.openBox<Todo>('todo');
   await Hive.openBox('friend');
   runApp(const MyApp());
 }
